075281733a81c7e3ee89a1371835d4f45618bcf4
LearXD <gutyxdcanal@gmail.com>
2023-12-10 14:03:55 -0300

    Builded

---
+++ dist/live-chat.d.ts
@@ -0,0 +1,15 @@
+/// <reference types="node" />
+import { EventEmitter } from "events";
+import { YoutubeId } from "./types/data";
+/**
+ * YouTubeライブチャット取得イベント
+ */
+export declare class LiveChat extends EventEmitter {
+    #private;
+    liveId?: string;
+    constructor(id: YoutubeId, interval?: number);
+    pause(): Promise<void>;
+    resume(): Promise<void>;
+    start(): Promise<boolean>;
+    stop(reason?: string): void;
+}
